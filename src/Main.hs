348023aba408c450a813217904db70655d6f8cf6
codec-abc <viot.camille@outlook.com>
2016-08-03 22:06:46 +0200

    Add more robustness to PolishNotationEvaluator

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,23 @@
 module Main where
-    import Triangle
-    import PascalTriangle
-    import Data.List
-    import TransposeLines
-    import Challenge263
-    
-    iter = PascalTriangle.getIter
-    baseTri = PascalTriangle.basePascalTriangle
-    transposed = TransposeLines.transpose ["Some","Text."]    
-    main = let
-        z = iter 5 baseTri in
-            do
-                -- putStrLn $ show transposed
-                -- putStrLn $ show z
-                -- putStrLn $ show Triangle.firstTri
-                -- putStrLn $ show Triangle.secondTri
-                -- putStrLn $ show Triangle.thirdTri
-                putStr ""
+
+import Triangle
+import PascalTriangle
+import Data.List
+import TransposeLines
+import Challenge263
+import PolishNotationEvaluator
+
+someString = "- 5 * 6 7"
+someString2 = "- * / 15 - 7 + 1 1 3 + 2 + 1 1"
+someString3 = "* / 15 - 7 + 1 1 3 + 2 + 1 1"
+someString4 = "* / 15 - 7 + 1 1 3 + 2 + 1"
+someString5 = "* 15 - 7 + 1 1 3 + 2 + 1 1"
+
+main = do
+    putStrLn ( show ( evaluate someString))
+    putStrLn ( show ( evaluate someString2))
+    putStrLn ( show ( evaluate someString3))
+    putStrLn ( show ( evaluate someString4))
+    putStrLn ( show ( evaluate someString5))
+
 
--- src/PolishNotationEvaluator.hs
+++ src/PolishNotationEvaluator.hs
@@ -1,60 +1,77 @@
-module PolishEvaluator (evaluate, someString, someString2) where
+module PolishNotationEvaluator (evaluate) where
 
 import Data.Maybe (isNothing, fromJust)
 import Data.Either (lefts, rights, isRight)
 import Text.Read (readMaybe)
 
---https://en.wikipedia.org/wiki/Polish_notation
+--https://en.wikipedia.org/wiki/PolishNotation_notation
 
-someString = "- 5 * 6 7"
-someString2 = "- * / 15 - 7 + 1 1 3 + 2 + 1 1"
+data PolishNotationSymbol = PolishNotationOperator ((Double -> Double -> Double), String) | PolishNotationOperand (Double)
+type PolishNotationParseResult = Either PolishNotationSymbol String
 
-data PolishSymbol = PolishOperator ((Double -> Double -> Double), String) | PolishOperand (Double)
-type PolishParseResult = Either PolishSymbol String
-
-instance Show PolishSymbol where
+instance Show PolishNotationSymbol where
     show w = case w of
-        PolishOperator (x,name) -> name
-        PolishOperand operand -> (show operand)
+        PolishNotationOperator (x,name) -> name
+        PolishNotationOperand operand -> (show operand)
 
+type Stack = [Double]
+type PolishNotationEvaluationState = ([PolishNotationSymbol], Stack)
+type PolishNotationStepResult = Either PolishNotationEvaluationState String
 
 -- todo : handle empty stack, empty operators with multiple numbers
-reduce :: [PolishSymbol] -> [Double] -> Either Double String
-
-reduce (firstSymbol:otherSymbols) numbers =
+step :: PolishNotationEvaluationState -> PolishNotationStepResult
+step (firstSymbol:otherSymbols, stack) =
     case firstSymbol of
-        PolishOperator (operator, name) -> let
-            hasEnoughNumbers = (length numbers) >= 2 in
-            if hasEnoughNumbers then let
-                first = numbers !! 0
-                second = numbers !! 1
+        PolishNotationOperator (operator, name) -> let
+            hasEnoughstack = (length stack) >= 2 in
+            if hasEnoughstack then let
+                first = stack !! 0
+                second = stack !! 1
                 result = operator first second in
-                reduce otherSymbols $ [result] ++ (drop 2 numbers)
+                Left $ (otherSymbols, [result] ++ (drop 2 stack))
             else
-                Right $ "There is not enough numbers when trying to apply function " ++ name
-        PolishOperand operand -> reduce otherSymbols ([operand] ++ numbers)
+                Right $ "There is not enough stack when trying to apply function " ++ name ++ " ."
+        PolishNotationOperand operand -> Left (otherSymbols, ([operand] ++ stack))
 
-reduce [] numbers =
-    let nb = length numbers in
+step ([] , stack) =
+    let nb = length stack in
         if nb == 1 then
-            Left $ head numbers
+            Left ([] , stack)
         else
-            Right $ "There is no more operator and operand and the stack contains " ++ (show nb) ++ " elements."
+            Right $ "There is no more operator or operand and the stack contains " ++ (show nb) ++ " elements."
 
-convertStringToPolishSymbol :: String -> PolishParseResult
-convertStringToPolishSymbol aString =
+isDone :: PolishNotationEvaluationState -> Bool
+isDone (polishSymbols, stack) = 
+    ((length polishSymbols) == 0) && ((length stack) == 1)
+
+run2 :: PolishNotationEvaluationState -> Int -> Either Double String
+run2 a b = 
+    let iter = step a in
+        case iter of
+            Left (polishSymbols, stack) -> 
+                if isDone (polishSymbols, stack) then
+                    Left $ head stack
+                else
+                    run2 (polishSymbols, stack) $ b + 1
+            Right error -> Right (error ++ " At iteration " ++ (show b))
+
+run :: PolishNotationEvaluationState -> Either Double String     
+run a = run2 a 0
+
+convertStringToPolishNotationSymbol :: String -> PolishNotationParseResult
+convertStringToPolishNotationSymbol aString =
     case aString of
-        "+"  -> Left $ PolishOperator ((+), "+")
-        "-"  -> Left $ PolishOperator ((-), "-")
-        "*"  -> Left $ PolishOperator ((*), "*")
-        "/"  -> Left $ PolishOperator ((/), "/")
-        aStr -> let maybeSymbol = (fmap) PolishOperand ((readMaybe aStr)::Maybe Double) in
+        "+"  -> Left $ PolishNotationOperator ((+), "(+)")
+        "-"  -> Left $ PolishNotationOperator ((-), "(-)")
+        "*"  -> Left $ PolishNotationOperator ((*), "(*)")
+        "/"  -> Left $ PolishNotationOperator ((/), "(/)")
+        aStr -> let maybeSymbol = (fmap) PolishNotationOperand ((readMaybe aStr)::Maybe Double) in
             case maybeSymbol of
                 Just x -> Left $ x
                 Nothing -> Right $ "Cannot parse token " ++ aStr
 
-parseInput :: String -> [PolishParseResult]
-parseInput aString = reverse $ map convertStringToPolishSymbol $ words aString
+parseInput :: String -> [PolishNotationParseResult]
+parseInput aString = reverse $ map convertStringToPolishNotationSymbol $ words aString
 
 evaluate :: String -> Either Double String
 evaluate inputString = let
@@ -62,6 +79,6 @@ evaluate inputString = let
     isOk = not (any isRight parseResult) in
     if isOk then
         let filteredResult = lefts parseResult in
-            reduce filteredResult  []
+            run (filteredResult, [])
     else
         Right $ head $ rights parseResult
